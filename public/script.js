85daa017a2d67e446815dd55737033e1cbaa8d33
Santiago <127254370+S4nt1ag@users.noreply.github.com>
2024-02-01 23:06:06 -0300

    update

--- public/script.js
+++ public/script.js
@@ -5,16 +5,26 @@ const chat = document.getElementById('mensagens');
 const socket = io();
 
 botaoEnviar.addEventListener('click', () => {
+    enviarMensagem();
+});
+
+caixaMensagem.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        enviarMensagem();
+    }
+});
+
+function enviarMensagem() {
     if (caixaMensagem.value !== '') {
         socket.emit('nova mensagem', caixaMensagem.value);
         caixaMensagem.value = '';
     }
-})
-
+}
 
 socket.addEventListener('nova mensagem', (msg) => {
-    const elementoMensagem = document.createElement('li'); //<li></li>
+    const elementoMensagem = document.createElement('li');
     elementoMensagem.textContent = msg;
-    elementoMensagem.classList.add('mensagem');// <li className="mensagem"></li>
+    elementoMensagem.classList.add('mensagem');
     chat.appendChild(elementoMensagem);
-})
+});
